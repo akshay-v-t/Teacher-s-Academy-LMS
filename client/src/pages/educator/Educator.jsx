9b7d6c7dda56a770a91dd7379b592b644aef12d3
akshay-v-t <akshayvt0487@gmail.com>
2025-01-29 23:54:06 +0530

    first commit

---
+++ client/src/pages/educator/Educator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Educator = () => {
+  return (
+   <>
+   <h1>Educator Page</h1>
+   <div>
+
+    {<Outlet/>}
+
+   </div>
+   </>
+)
+}
+
+export default Educator
