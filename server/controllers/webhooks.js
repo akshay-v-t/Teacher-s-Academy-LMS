8e43592f78120f40ec76510d6a3813d933cdb86d
akshay-v-t <akshayvt0487@gmail.com>
2025-03-04 21:11:35 +0530

    correct webhook.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -20,13 +20,13 @@ export const clerkWebhooks = async (req, res)=>{
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
 
                 await User.create(userData)
-                res.JSON({})
+                res.json({})
                 break;
             }
 
@@ -40,7 +40,7 @@ export const clerkWebhooks = async (req, res)=>{
                 }
 
                 await User.findByIdAndUpdate(data.id, userData )
-                res.JSON({})
+                res.json({})
                 break
 
             }
