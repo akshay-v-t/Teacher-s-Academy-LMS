1d8e03ce88aaeb7baccab36a12ee4fe53b597a9d
akshay-v-t <akshayvt0487@gmail.com>
2025-03-04 21:23:09 +0530

    fix typo in webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -10,8 +10,9 @@ export const clerkWebhooks = async (req, res)=>{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-signature"]
+           "svix-id": req.headers["svix-id"],
+"svix-timestamp": req.headers["svix-timestamp"], 
+  "svix-signature": req.headers["svix-signature"],
         })
 
         const {data, type} = req.body
@@ -34,7 +35,7 @@ export const clerkWebhooks = async (req, res)=>{
 
                 const userData = {
                    
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
