30f2b6bb2a62f8fff422cbc9333caedd764b6cac
akshay-v-t <akshayvt0487@gmail.com>
2025-03-09 16:18:22 +0530

    Project complete

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,15 +1,47 @@
-import React, { useContext } from 'react'
+import  { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { AppContext } from '../../context/AppContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Navbar = () => {
-    const {navigate, isEducator} = useContext(AppContext)
+
+
+
+
+
+
+
+
+    const {navigate, isEducator, backendUrl, setisEducator, getToken} = useContext(AppContext)
 
     const isCourseListPage = location.pathname.includes('/course-list')
     const {openSignIn} = useClerk()
     const {user} = useUser()
+
+    const becomeEducator = async ()=>{
+        try{
+            if(isEducator){
+                navigate('/educator')
+                return;
+            }
+            const token = await getToken()
+            const {data} = await axios.get(backendUrl + 'api/educator/update-role',{
+                headers: {Authorization: `Bearer ${token}`}
+            }) 
+            if(data.success){
+                setisEducator(true)
+                toast.success(data.message)
+            }else{
+                toast.error(data.message)
+            }
+        }catch (error){
+            toast.error(error.message)
+
+        }
+    }
     return (
     
     <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
@@ -19,7 +51,7 @@ const Navbar = () => {
                 
                 { 
                 user && <>
-                <button onClick={()=> {navigate('/educator')}}>{ isEducator ? 'Educator Dashboard' : 'Become Educator'  }</button>
+                <button onClick={becomeEducator}>{ isEducator ? 'Educator Dashboard' : 'Become Educator'  }</button>
                 | <Link to='/my-enrollments'>My Enrollments</Link>
                 </>
                 }
@@ -44,7 +76,7 @@ const Navbar = () => {
                 { 
 
                     user && <>
-                    <button onClick={()=> {navigate('/educator')}}>{ isEducator ? 'Educator Dashboard' : 'Become Educator'  }</button>
+                    <button onClick={becomeEducator}>{ isEducator ? 'Educator Dashboard' : 'Become Educator'  }</button>
                     | <Link to='/my-enrollments'>My Enrollments</Link>
                     </>
                 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,11 +3,16 @@ import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
 import {useAuth, useUser} from '@clerk/clerk-react'
+import axios from 'axios'
+import { toast } from "react-toastify";
 
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
+
+    
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const currency = import.meta.env.VITE_CURRENCY
 
@@ -17,14 +22,52 @@ export const AppContextProvider = (props)=>{
     const {user} = useUser()
 
     const [allCourses, setAllCourses] = useState([])
-    const [isEducator, setisEducator] = useState(true)
+    const [isEducator, setisEducator] = useState(false)
     const [enrolledCourses, setEnrolledCourses] = useState([])
-
+    const [userData, setUserData] = useState(null)
+    
+    
 
     //Fetch all courses
 
     const fetchAllCourses = async () => {
-        setAllCourses(dummyCourses)
+      try {
+
+       const {data}= await axios.get(backendUrl + 'api/course/all')
+
+       if(data.success){
+        setAllCourses(data.courses)
+       }else{
+        toast.error(data.message)
+
+       }
+        
+      } catch (error) {
+
+        toast.error(error.message)
+        
+      }
+    }
+
+    //fetch userdata
+
+    const fetchUserData = async ()=>{
+        if(user.publicMetadata.role==='educator'){
+            setisEducator(true)
+        }
+        try {
+            const token = await getToken();
+           const {data} =  await axios.get(backendUrl + 'api/user/data', {headers: {Authorization: `Bearer ${token}`}})
+
+           if(data.success){
+            setUserData(data.user)
+           }else{
+            toast.error(data.message)
+           }
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
     }
 
     //Functiion to calculate average rating of course
@@ -37,7 +80,7 @@ export const AppContextProvider = (props)=>{
         course.courseRatings.forEach(rating => {
             totalRating += rating.rating
         })
-        return totalRating/ course.courseRatings.length
+        return Math.floor(totalRating/ course.courseRatings.length)
 
     }
 
@@ -71,31 +114,47 @@ export const AppContextProvider = (props)=>{
     //Fetch user enrolled courses
 
     const fetchUserEnrolledCourses = async () => {
-        setEnrolledCourses(dummyCourses)
+
+        try {
+
+            const token = await  getToken();
+       const {data} = await axios.get(backendUrl + 'api/user/enrolled-courses', {headers: {Authorization: `Bearer ${token}`}})
+
+       if(data.success){
+        setEnrolledCourses(data.enrolledCourses.reverse())
+       }else{
+        toast.error(data.message)
+       }
+            
+        } catch (error) {
+
+            toast.error(error.message)
+            
+        }
+       
     }
 
 
 
     useEffect(()=>{
         fetchAllCourses()
-        fetchUserEnrolledCourses()
+       
 
     },[])
 
-    const logToken = async ()=>{
-        console.log(await getToken());
-    }
-
+   
     useEffect(()=>{
         if(user){
 
-            logToken()
+         
+            fetchUserData()
+            fetchUserEnrolledCourses()
 
         }
     },[user])
 
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setisEducator, calculateNumberOfLectures, calculateCourseDuration, calculateChapterTime, setEnrolledCourses, enrolledCourses
+        currency, allCourses, navigate, calculateRating, isEducator, setisEducator, calculateNumberOfLectures, calculateCourseDuration, calculateChapterTime, setEnrolledCourses, enrolledCourses, backendUrl, userData,setUserData,getToken, fetchAllCourses, fetchUserEnrolledCourses
 
     }
     return(
